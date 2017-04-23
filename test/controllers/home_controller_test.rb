a511f114aedadee9b15323a949e6549051df6570
Truong Dung <checkraiser11@gmail.com>
2017-04-24 06:53:37 +0700

    Fix test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,6 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
     assert_response :success
-	assert_select 'title', "Welcome to Rails Testing Guide"
+	assert_select 'title', "CCS"
   end
 end
