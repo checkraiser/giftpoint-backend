5ac3c6d62d2c8c586d4aad763b788bfe26c4c4d3
Truong Dung <checkraiser11@gmail.com>
2017-04-24 06:41:57 +0700

    Landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   skip_before_action :require_login
+  layout :home_layout
   
   def index
+  end
+
+  private
+
+  def home_layout
+  	return "landing" unless logged_in?
+  	"application"
   end
 end
