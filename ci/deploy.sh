61f4a18b32358d0b8475696d09f0c156b85f4c84
Truong Dung <checkraiser11@gmail.com>
2017-04-24 05:40:09 +0700

    Add ci

---
+++ ci/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo docker pull checkraiser/giftpoint-backend
+sudo docker stop backend
+sudo docker rm backend 
+sudo docker run --name backend --link some-postgres:postgres -p 80:80 -e RAILS_SERVE_STATIC_FILES=$RAILS_SERVE_STATIC_FILES -e SECRET_KEY_BASE=$SECRET_KEY_BASE -e PORT=80 -e RAILS_ENV=production -d checkraiser/giftpoint-backend
+sudo docker exec backend bash -c 'RAILS_ENV=production bin/rails db:migrate'
